1e0a5411611c341043448321036f2662b610c8a6
Praveen Kumar Chandran <praveenhome@Praveens-MacBook-Pro.local>
2023-12-27 21:04:57 -0500

    training added

--- src/cnnClassifier/entity/__init__.py
+++ src/cnnClassifier/entity/__init__.py
@@ -1,3 +1,4 @@
 from cnnClassifier.entity.config_entity import (DataIngestionConfig)
 from cnnClassifier.entity.config_entity import (PrepareBaseModelConfig)
 from cnnClassifier.entity.config_entity import(PrepareCallbacksConfig)
+from cnnClassifier.entity.config_entity import(TrainingConfig)
